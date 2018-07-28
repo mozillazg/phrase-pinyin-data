bd097faf22ae1fe3601b6cf50f36e16ca63ea41c
mozillazg <mozillazg101@gmail.com>
2018-07-28 15:53:24 +0800

    Bump version: 0.8.0 → 0.8.1

--- merge.py
+++ merge.py
@@ -33,7 +33,7 @@ def sort(pinyin_d):
 
 
 def output(pinyin_s):
-    print('# version: 0.8.0')
+    print('# version: 0.8.1')
     print('# source: https://github.com/mozillazg/phrase-pinyin-data')
     for hanzi, pinyin in pinyin_s:
         print('{hanzi}: {pinyin}'.format(hanzi=hanzi, pinyin=pinyin))
